5433da198fece24300f193a405b2cd1db7dc0d78
Thai Tai Tran <t.tran@apriljune.com>
2021-09-06 13:44:22 +0200

    1) finalized output of summary (price)

--- Typemaster/js/keyboard-customization.js
+++ Typemaster/js/keyboard-customization.js
@@ -1,3 +1,4 @@
+// global values
 const figures = document.querySelectorAll('figure');
 const custLayout = document.querySelector("#custom-layout");
 const custSwitch = document.querySelector("#custom-switch");
@@ -5,172 +6,188 @@ const custExtraM = document.querySelector("#custom-extra-mouse");
 const custExtraO = document.querySelector("#custom-extra-other");
 const confSelBtn = document.querySelector("#confirm-select-btn");
 
-for (let figure of figures) {
-    figure.addEventListener("click", function (e) {
+// toggle highlight
+    for (let figure of figures) {
+        figure.addEventListener("click", function (e) {
 
-        const layoutId = 'custom-layout'
-        const switchId = 'custom-switch'
-        const extraMId = 'custom-extra-mouse'
-        const extraOId = "custom-extra-other"
+            const layoutId = 'custom-layout'
+            const switchId = 'custom-switch'
+            const extraMId = 'custom-extra-mouse'
+            const extraOId = "custom-extra-other"
 
 
-        selectionSingle(e, custLayout, layoutId);
-        selectionSingle(e, custSwitch, switchId);
-        selectionSingle(e, custExtraM, extraMId);
+            selectionSingle(e, custLayout, layoutId);
+            selectionSingle(e, custSwitch, switchId);
+            selectionSingle(e, custExtraM, extraMId);
 
-        if ((isDescendant(e.target, extraOId) && (e.target.parentNode.classList.contains("img-box")))) {
+            if ((isDescendant(e.target, extraOId) && (e.target.parentNode.classList.contains("img-box")))) {
+                e.target.parentNode.classList.toggle("img-highlight");
+            }
+
+        })
+    }
+
+    // Sub-Functions
+
+    const isDescendant = (el, parentId) => {
+        let isChild = false
+
+        if (el.id === parentId) { //is this the element itself?
+            isChild = false
+        }
+
+        while (el = el.parentNode) {
+            if (el.id == parentId) {
+                isChild = true
+            }
+        }
+
+        return isChild
+    }
+
+    function selectionSingle (e, container, parentId) {
+
+        if ((isDescendant(e.target, parentId)) && (e.target.parentNode.classList.contains("img-box"))) {
+
+            for (let i = 0; i < container.children.length; i++) {
+                container.children[i].classList.remove('img-highlight')
+            }
             e.target.parentNode.classList.toggle("img-highlight");
-        }
-
-    })
-}
-
-const isDescendant = (el, parentId) => {
-    let isChild = false
-
-    if (el.id === parentId) { //is this the element itself?
-        isChild = false
-    }
-
-    while (el = el.parentNode) {
-        if (el.id == parentId) {
-            isChild = true
-        }
-    }
-
-    return isChild
-}
-
-function selectionSingle (e, container, parentId) {
-
-    if ((isDescendant(e.target, parentId)) && (e.target.parentNode.classList.contains("img-box"))) {
-
-        for (let i = 0; i < container.children.length; i++) {
-            container.children[i].classList.remove('img-highlight')
-        }
-        e.target.parentNode.classList.toggle("img-highlight");
-        if (selectionComplete()) {
-            confSelBtn.classList.remove("grayed-out");
-        }else {
-            confSelBtn.classList.add("grayed-out");
-        }
-
-    }
-
-}
-
-function selectionComplete () {
-
-    let checkLayout = false;
-    let checkSwitch = false;
-    let checkExtraM = false;
-
-        for (let element of custLayout.children) {
-            if (element.classList.contains("img-highlight")) {
-                checkLayout = true;
-            }
-        }
-
-        for (let element of custSwitch.children) {
-            if (element.classList.contains("img-highlight")) {
-                checkSwitch = true;
-            }
-        }
-
-        for (let element of custExtraM.children) {
-            if(element.classList.contains("img-highlight")) {
-                checkExtraM = true;
+            if (selectionComplete()) {
+                confSelBtn.classList.remove("grayed-out");
+            }else {
+                confSelBtn.classList.add("grayed-out");
             }
 
         }
 
-        if((checkLayout) && (checkSwitch) &&(checkExtraM)) {
-            return true;
-        }
-}
-
-function summarizeOrder () {
-    const custContainer = document.querySelector(".section-custom");
-    let unSelectedOptions = custContainer.querySelectorAll("figure:not(.img-highlight)");
-
-    let allOptionsLay = custLayout.querySelectorAll("figure");
-    let allOptionsSw = custSwitch.querySelectorAll("figure");
-    let allOptionsEM = custExtraM.querySelectorAll("figure");
-    let allOptionsEO = custExtraO.querySelectorAll("figure");
-
-
-    const custHeading = document.querySelector(".heading-custom");
-
-    for (let element of unSelectedOptions) {
-        element.classList.add("inactive");
     }
-    summaryMoveLeft(allOptionsLay);
-    summaryMoveLeft(allOptionsSw);
-    summaryMoveLeft(allOptionsEM);
-    summaryMoveLeftEO();
-    confSelBtn.classList.add("grayed-out");
-    custHeading.innerText = "Your Selection";
-    confSelBtn.innerText="Your total is: " + calcPrice() +"€";
 
-    let node = document.createElement("DIV");
-    let textnode = document.createTextNode("*Base Price 200€ + Extras");
-    node.appendChild(textnode);
-    confSelBtn.appendChild(node);
+    function selectionComplete () {
 
+        let checkLayout = false;
+        let checkSwitch = false;
+        let checkExtraM = false;
 
-
-    function summaryMoveLeft (allOptions) {
-        for (let element of allOptions) {
-            let i = Array.from(allOptions).indexOf(element)
-            let x = (i * -100);
-            let y = (i * -2)
-            if (element.classList.contains("img-highlight")) {
-                element.style.transform = "translateX(" + x + "%) translateX(" + y + "rem)";
+            for (let element of custLayout.children) {
+                if (element.classList.contains("img-highlight")) {
+                    checkLayout = true;
+                }
             }
 
-        }
-    }
-
-    function summaryMoveLeftEO () {
-        let optionsArray = Array.from(allOptionsEO)
-        if ((optionsArray[0].classList.contains("img-highlight")) && (optionsArray[1].classList.contains("img-highlight"))) {
-            for (let element of allOptionsEO) {
-                element.style.transform = "translateX(-100%) translateX(-2rem)";
+            for (let element of custSwitch.children) {
+                if (element.classList.contains("img-highlight")) {
+                    checkSwitch = true;
+                }
             }
-            // let i = Array.from(allOptionsEO).indexOf(element)
-        }else if ((optionsArray[0].classList.contains("img-highlight")) && !(optionsArray[1].classList.contains("img-highlight")))  {
-            optionsArray[0].style.transform = "translateX(-100%) translateX(-2rem)";
 
-        }else if (!(optionsArray[0].classList.contains("img-highlight")) && (optionsArray[1].classList.contains("img-highlight")))  {
-            optionsArray[1].style.transform = "translateX(-200%) translateX(-4rem)";
-        }
+            for (let element of custExtraM.children) {
+                if(element.classList.contains("img-highlight")) {
+                    checkExtraM = true;
+                }
+
+            }
+
+            if((checkLayout) && (checkSwitch) &&(checkExtraM)) {
+                return true;
+            }
     }
 
-    function calcPrice () {
+//Summary
 
-        let selectedOptions = custContainer.querySelectorAll("figure.img-highlight");
+    function summarizeOrder () {
+        //local Variables
+        const custContainer = document.querySelector(".section-custom");
+        const summaryContainer = document.querySelector(".summary-row");
+        let unSelectedOptions = custContainer.querySelectorAll("figure:not(.img-highlight)");
 
-        let WirelessMousePrice = 0;
-        let WiredMousePrice = 0;
-        let MousepadPrice = 0;
-        let LightingPrice = 0;
 
-        WirelessMousePrice = priceOfSelected(allOptionsEM[0]);
-        WiredMousePrice = priceOfSelected(allOptionsEM[1]);
-        MousepadPrice = priceOfSelected(allOptionsEO[0]);
-        LightingPrice = priceOfSelected(allOptionsEO[1]);
+        let allOptionsLay = custLayout.querySelectorAll("figure");
+        let allOptionsSw = custSwitch.querySelectorAll("figure");
+        let allOptionsEM = custExtraM.querySelectorAll("figure");
+        let allOptionsEO = custExtraO.querySelectorAll("figure");
 
-        function priceOfSelected(target) {
-            if(Array.from(selectedOptions).includes(target)) {
-                return parseFloat(target.dataset.price);
-            } else {
-                return parseFloat("0");
+
+        const custHeading = document.querySelector(".heading-custom");
+
+        // main function block
+        for (let element of unSelectedOptions) {
+            element.classList.add("inactive");
+        }
+        summaryMoveLeft(allOptionsLay);
+        summaryMoveLeft(allOptionsSw);
+        summaryMoveLeft(allOptionsEM);
+        summaryMoveLeftEO();
+        confSelBtn.classList.add("inactive");
+        custHeading.innerText = "Your Selection";
+
+        // add Summary Texts
+        let node1 = document.createElement("DIV");
+        node1.classList.add("summary-text")
+        let textnode1 = document.createTextNode("Your total is: " + calcPrice() +"€");
+        node1.appendChild(textnode1);
+        summaryContainer.appendChild(node1);
+
+        let node2 = document.createElement("SPAN");
+        node2.classList.add("summary-sub-text")
+        let textnode2 = document.createTextNode("*Base Price 200€ + Extras");
+        node2.appendChild(textnode2);
+        summaryContainer.appendChild(node2);
+
+
+        // Sub-Functions
+        function summaryMoveLeft (allOptions) {
+            for (let element of allOptions) {
+                let i = Array.from(allOptions).indexOf(element)
+                let x = (i * -100);
+                let y = (i * -2)
+                if (element.classList.contains("img-highlight")) {
+                    element.style.transform = "translateX(" + x + "%) translateX(" + y + "rem)";
+                }
+
             }
         }
 
-        return 200 + WirelessMousePrice + WiredMousePrice + MousepadPrice +LightingPrice;
+        function summaryMoveLeftEO () {
+            let optionsArray = Array.from(allOptionsEO)
+            if ((optionsArray[0].classList.contains("img-highlight")) && (optionsArray[1].classList.contains("img-highlight"))) {
+                for (let element of allOptionsEO) {
+                    element.style.transform = "translateX(-100%) translateX(-2rem)";
+                }
+                // let i = Array.from(allOptionsEO).indexOf(element)
+            }else if ((optionsArray[0].classList.contains("img-highlight")) && !(optionsArray[1].classList.contains("img-highlight")))  {
+                optionsArray[0].style.transform = "translateX(-100%) translateX(-2rem)";
+
+            }else if (!(optionsArray[0].classList.contains("img-highlight")) && (optionsArray[1].classList.contains("img-highlight")))  {
+                optionsArray[1].style.transform = "translateX(-200%) translateX(-4rem)";
+            }
+        }
+
+        function calcPrice () {
+
+            let selectedOptions = custContainer.querySelectorAll("figure.img-highlight");
+
+            let WirelessMousePrice = 0;
+            let WiredMousePrice = 0;
+            let MousepadPrice = 0;
+            let LightingPrice = 0;
+
+            WirelessMousePrice = priceOfSelected(allOptionsEM[0]);
+            WiredMousePrice = priceOfSelected(allOptionsEM[1]);
+            MousepadPrice = priceOfSelected(allOptionsEO[0]);
+            LightingPrice = priceOfSelected(allOptionsEO[1]);
+
+            function priceOfSelected(target) {
+                if(Array.from(selectedOptions).includes(target)) {
+                    return parseFloat(target.dataset.price);
+                } else {
+                    return parseFloat("0");
+                }
+            }
+
+            return 200 + WirelessMousePrice + WiredMousePrice + MousepadPrice +LightingPrice;
+        }
     }
-}
 
 
 
